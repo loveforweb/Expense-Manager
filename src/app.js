7609a31d3d15cad80ddfb30a21955f33293c79f2
Jag Chumber <jag@loveforweb.com>
2018-02-15 17:27:45 +0000

    Added firebase auth base

--- src/app.js
+++ src/app.js
@@ -10,8 +10,7 @@ import getVisibleExpenses from './selectors/expenses';
 import 'react-dates/lib/css/_datepicker.css';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
-import './firebase/firebase';
-import './playground/promises';
+import {firebase } from './firebase/firebase';
 
 const store = configureStore();
 
@@ -25,4 +24,12 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 store.dispatch(startSetExpenses()).then(() => {
     ReactDOM.render(jsx, document.getElementById('app'));
+});
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        console.log('logged in');
+    } else {
+        console.log('logged out');
+    }
 });
